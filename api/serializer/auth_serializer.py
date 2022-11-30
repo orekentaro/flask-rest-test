2d24617ed781dd3cc0b93937c87596c96e0672d9
orekentaro <kntru0218gj@gmail.com>
2022-12-01 01:49:04 +0900

    修正

--- api/serializer/auth_serializer.py
+++ api/serializer/auth_serializer.py
@@ -3,15 +3,13 @@ from typing import Any, Optional
 from models.auth_master import AuthMaster
 from models.user_master import UserMaster
 from serializer.base_serializer import BaseSerializer
-from utils.get_data import get_one
 
 
 class AuthSerializer(BaseSerializer):
-    @classmethod
-    def serialize(cls, user: Optional[UserMaster]) -> dict[str, Any]:  # type: ignore[override]
+    def data(self, user: Optional[UserMaster]) -> dict[str, Any]:  # type: ignore[override]
         if user is None:
             return {}
-        auth = get_one(AuthMaster, user.auth_id)
+        auth = self._get_one(AuthMaster, user.auth_id)
         user_data = user.__dict__
         user_data.pop("_sa_instance_state")
         user_data.update(auth_name=auth.get("auth", ""))
