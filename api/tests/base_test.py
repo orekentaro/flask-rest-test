15298d29e5db338d5004a18a52dcfd4c09336e3b
orekentaro <kntru0218gj@gmail.com>
2022-12-01 01:47:45 +0900

    不要ファイル削除

--- api/tests/base_test.py
+++
@@ -1,6 +0,0 @@
-class BaseTest:
-    def login(self, client):
-        with client:
-            client.post(
-                "/auth/", data={"email": "test@test.test", "password": "password123"}
-            )
